8286ffd6272facfee6e9f6512e96f495637acb8b
Emma Nasseri <enasseri02@gmail.com>
2023-04-17 11:58:01 -0500

    rearranged components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ClaimComponent from "./components/NewClaimComponent";
+import ClaimComponent from "./pages/NewClaimComponent";
 import MovingCircles from "./components/MovingCircles";
 import DataLogSidebar from "./components/DataLogSidebar";
-import GenerateComponent from "./components/GenerateComponent";
+import GenerateComponent from "./pages/GenerateComponent";
+import About from "./pages/About";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
@@ -14,35 +15,36 @@ const App = () => {
           with the Spencer Museum of Art
         </header>
 
-        <div style={{...appStyles.contentContainer, borderBottom: "1px solid black"}}>
-          <div
-            style={{
-              ...appStyles.section,
-              marginBottom: "10px",
-              width: "100%"
-            }}
-          >
+        <div style={appStyles.contentContainer}>
+          <div style={appStyles.leftSection}>
+            <div style={appStyles.navContainer}>
+              <Link to="/" style={appStyles.navLink}>
+                About
+              </Link>
+              <Link to="/generate" style={appStyles.navLink}>
+                Generate
+              </Link>
+              <Link to="/claim" style={appStyles.navLink}>
+                Claim
+              </Link>
+            </div>
+            <div style={appStyles.content}>
+              <Routes>
+                <Route path="/" element={<About styles={appStyles} />} />
+                <Route
+                  path="/generate"
+                  element={<GenerateComponent styles={appStyles} />}
+                />
+                <Route
+                  path="/claim"
+                  element={<ClaimComponent styles={appStyles} />}
+                />
+              </Routes>
+            </div>
+          </div>
+          <div style={appStyles.rightSection}>
             <MovingCircles styles={appStyles} />
-          </div>
-          <div style={appStyles.nav}>
-            <Link to="/generate" style={appStyles.navLink}>
-              Generate
-            </Link>
-            <Link to="/claim" style={appStyles.navLink}>
-              Claim
-            </Link>
-          </div>
-          <div style={{ ...appStyles.section, width: "75%" }}>
-            <Routes>
-              <Route
-                path="/generate"
-                element={<GenerateComponent styles={appStyles} />}
-              />
-              <Route
-                path="/claim"
-                element={<ClaimComponent styles={appStyles} />}
-              />
-            </Routes>
+            <DataLogSidebar styles={appStyles} />
           </div>
         </div>
       </div>
@@ -51,22 +53,6 @@ const App = () => {
 };
 
 const appStyles = {
-  nav: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: "10px 0",
-  },
-  navLink: {
-    textDecoration: "none",
-    color: "#000",
-    padding: "10px 20px",
-    margin: "0 10px",
-    backgroundColor: "#BFEFFF",
-    borderRadius: "5px",
-    fontWeight: "bold",
-    outline: "2px solid #000000",
-  },
   container: {
     display: "flex",
     flexDirection: "column",
@@ -85,54 +71,82 @@ const appStyles = {
   },
   contentContainer: {
     display: "flex",
+    flexDirection: "row",
     width: "100%",
     justifyContent: "center",
     alignItems: "flex-start",
   },
-  section: {
-    padding: "20px",
+  leftSection: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "50%",
+  },
+  rightSection: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "50%",
+  },
+  navContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "10px 0",
+  },
+  navLink: {
+    textDecoration: "none",
+    color: "#000",
+    padding: "10px 20px",
+    margin: "0 10px",
+    backgroundColor: "#BFEFFF",
+    borderRadius: "5px",
+    fontWeight: "bold",
+    outline: "2px solid #000000",
+    },
+    content: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     width: "100%",
-  },
-  circleContainer: {
+    },
+    circleContainer: {
     display: "flex",
     justifyContent: "center",
     marginBottom: "10px",
-  },
-  circle: {
+    },
+    circle: {
     width: "40px",
     height: "40px",
     borderRadius: "50%",
     backgroundColor: "#F9C2FF",
     margin: "0 10px",
     outline: "2px solid #000000",
-  },
-  rectangle: {
+    },
+    rectangle: {
     width: "200px",
     height: "50px",
     backgroundColor: "#BFEFFF",
     outline: "2px solid #000000",
-  },
-  title: {
+    },
+    title: {
     margin: "0 0 10px 0",
     fontWeight: "bold",
-  },
-  button: {
+    },
+    button: {
     padding: "10px 20px",
     backgroundColor: "#FFD1A4",
     borderRadius: "20px",
     border: "none",
     fontWeight: "bold",
     outline: "2px solid #000000",
-  },
-  sidebar: {
+    },
+    sidebar: {
     position: "relative",
     width: "25%",
     backgroundColor: "#e6f9ff",
     padding: "20px",
-  },
-};
-
-export default App;
+    },
+    };
+    
+    export default App;
