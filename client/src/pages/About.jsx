ef598e6b47fb1a7717b711debf750788b2438997
Emma Nasseri <enasseri02@gmail.com>
2023-04-17 12:06:59 -0500

    updated about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,11 @@
 const About = ({ styles }) => {
   return (
     <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
+      <h1>About the Project</h1>
+      <p>This project from the University of Kansas was done in collaboration with the Spencer Museum of Art, the Institute for Information Sciences, and artists Simon Denny and Stephanie Dinkins. </p>
+      <p>This project explores ideas of ownership in digital spaces by the distribution of claim NFTs and examination of the behavior of their owners.</p>
+      <h1>About this System</h1>
+      <p>This system enables the generation of wallets distribution of Claim NFTs into them. Click the Generate page to see how wallets are generated and distributed. Wallet owners can then click the Claim button to verify that they are an owner and attempt to claim land. </p>
     </div>
   );
 }
