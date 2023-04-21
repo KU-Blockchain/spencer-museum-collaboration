62a06319e5f813e28c174258fcd9082266f9b677
Emma Nasseri <enasseri02@gmail.com>
2023-04-20 19:38:50 -0500

    set up part of database connection

--- server/app.js
+++ server/app.js
@@ -2,11 +2,55 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+const dbPassword = process.env.DB_PASSWORD;
+
+// Replace <password> with your actual password
+const uri = "mongodb+srv://enasseri02:{password}@cluster0.uxmku1l.mongodb.net/myDatabase?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    // Now that we have connected, start the express server
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    // Connect Mongoose to the MongoDB client
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('Mongoose connected'))
+    .catch((err) => console.log('Error connecting Mongoose:', err));
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  } finally {
+    // Don't close the client, as we want to keep it connected for the lifetime of the server
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
